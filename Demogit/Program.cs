c3ec84fd3cd00bb7d7f1194d8e46592f2f0b57cb
quangbui2kk3 <quangbui2kk3@gmail.com>
2024-12-21 23:39:40 +0700

    add hello

--- Demogit/Program.cs
+++ Demogit/Program.cs
@@ -11,6 +11,7 @@ namespace Demogit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word");
+            Console.WriteLine(" Hello anh em");
             Console.ReadLine();
         }
     }
